0c80aa93691faede66febc492b25b667837cb4af
Dave <dsrour@eng.ucsd.edu>
2018-02-10 15:21:17 -0800

    - Added some generic global css - Created a split component for splitting logic - Added a gitignore for visual studio artifacts

--- ReqSplit/src/app/app.module.ts
+++ ReqSplit/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SplitComponent } from './split/split.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplitComponent
   ],
   imports: [
     BrowserModule

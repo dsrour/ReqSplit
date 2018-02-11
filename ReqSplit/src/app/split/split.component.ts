c6aa26bc618d7d6d4e0b729e810ef44816b6b758
Dave <dsrour@eng.ucsd.edu>
2018-02-10 21:54:41 -0800

    Populating contributors list when choose num of contributors

--- ReqSplit/src/app/split/split.component.ts
+++ ReqSplit/src/app/split/split.component.ts
@@ -10,11 +10,26 @@ import { Contributor } from '../contributor';
 export class SplitComponent implements OnInit {
     private _total = 0;
     private _numContributors = 0;
-    private _contributors: Contributor[];
-    
+    private _contributors = [];
+
     constructor() { }
 
-    ngOnInit() {        
+    ngOnInit() {
+
     }
 
+    generateContributorsList(newValue: number) {
+        let cur_length = this._contributors.length;
+        if (cur_length < newValue) {
+            for (let i = this._contributors.length; i < newValue; i++) {
+                var a: Contributor = { id: i, name: 'Mr. ' + i, contributionPercentage: 0.0 };
+                this._contributors.push(a);
+            }
+        }
+        else {
+            for (let i = newValue; i < cur_length; i++) {
+                this._contributors.pop();
+            }
+        }
+    }
 }
